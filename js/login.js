328d1afdea6da7770f22776ec570f2527eac620a
TaulantiUbt <talifazliu@gmail.com>
2020-05-17 01:08:19 +0200

    komiti i radhes

--- js/login.js
+++ js/login.js
@@ -9,3 +9,18 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+function validimi(form) {
+	let inputs = document.querySelectorAll("input");
+	// const paswordi=document.getElementById('password');
+	// const name=document.getElementById('uname');
+
+    if (form == 0 && inputs[2].value == "" && inputs[3].value == "" && inputs[4].value == "") {
+			alert("Can't Sign Un , fields can not be blank! Fill the fields...");
+    } else if (form == 1 && inputs[0].value == "" && inputs[1].value == "") {
+			alert("Can't Sign In , fields can not be blank! Fill the fields...");
+	}
+	
+	
+}
